84e9672379e9e91ecb7187f3ec98534063e1067e
IrisBP <irisbapi@pm.me>
2026-08-28 16:47:38 +0200

    added missing ref file

--- python_scripts/P3_description.py
+++ python_scripts/P3_description.py
@@ -29,7 +29,7 @@ def get_fluo_info(V):
     m[m == c] = 1
     return [int(c)], list(get_dist(m, G)), list(get_dist(m, R)), list(get_dist(m, FR))
    
-def describe(cell_masks, nucleus_masks, t, thread_count, g,r,fr):
+def describe(cell_masks, nucleus_masks, t, thread_count, g, r , fr , ref_table):
     '''
     for each cell in the frame, get the area, centroid coordinate, intensity distribution for both cell and nucleus
     Output the information as a table 
@@ -41,51 +41,62 @@ def describe(cell_masks, nucleus_masks, t, thread_count, g,r,fr):
     G=tif.imread(g)
     R=tif.imread(r)
     FR=tif.imread(fr)
-
-    # get basic cell and nuclear feature (size and coordinates)
-    cell_feat, ix_to_cell2 = hu.get_features(c_mask, t)
-    nuclear_feat, ix_to_cell2 = hu.get_features(n_mask, t)
-    # rename the column names of the nuclear table 
-    nuclear_feat.rename(columns={'sqrtarea': 'N_sqrtarea', 'area': 'N_area','com_x': 'N_com_x','com_y': 'N_com_y' }, inplace=True)
-    nuclear_feat=nuclear_feat.drop(["time"], axis=1)
-    # merge the cell and nuclear tables into 1 
-    results = pd.merge(cell_feat, nuclear_feat, on="cell", how="left")
-
-    # using multiprocessing parallelization, extract the fluo information for every cell in the masks 
-    with Pool(thread_count) as p:
-        list_V=[(results['cell'][i], c_mask, G, R, FR) for i in range(len(results))]
-        infos=p.map(get_fluo_info, list_V ) #get fluo info ouput mean/max/std for all 3 channels 
-        infos_table=np.array([[j for i in infos[k] for j in i] for k in range(len(infos))])
-        #update the table 
-        infos_table_cell = pd.DataFrame(data=infos_table, columns=['cell','G_mean','G_max','G_std','R_mean', 'R_max','R_std', 'Fr_mean', 'Fr_max', 'Fr_std'])
-        #do the same thing with the nuclear masks 
-        list_V=[(results['cell'][i], n_mask, G, R, FR) for i in range(len(results))]
-        infos=p.map(get_fluo_info, list_V )
-        infos_table=np.array([[j for i in infos[k] for j in i] for k in range(len(infos))])
-        infos_table_nucleus = pd.DataFrame(data=infos_table, columns=['cell','N_G_mean','N_G_max','N_G_std','N_R_mean', 'N_R_max','N_R_std', 'N_Fr_mean', 'N_Fr_max', 'N_Fr_std'])
-
-        #merge all the tables 
-        results = pd.merge(results, infos_table_cell, on="cell", how="left")
-        results = pd.merge(results, infos_table_nucleus, on="cell", how="left")
+    if np.max(G)>1: # the image isn't empty 
        
+
+        # get basic cell and nuclear feature (size and coordinates)
+        cell_feat, ix_to_cell2 = hu.get_features(c_mask, t)
+        nuclear_feat, ix_to_cell2 = hu.get_features(n_mask, t)
+        # rename the column names of the nuclear table 
+        nuclear_feat.rename(columns={'sqrtarea': 'N_sqrtarea', 'area': 'N_area','com_x': 'N_com_x','com_y': 'N_com_y' }, inplace=True)
+        nuclear_feat=nuclear_feat.drop(["time"], axis=1)
+        # merge the cell and nuclear tables into 1 
+        results = pd.merge(cell_feat, nuclear_feat, on="cell", how="left")
+
+        # using multiprocessing parallelization, extract the fluo information for every cell in the masks 
+        with Pool(thread_count) as p:
+            list_V=[(results['cell'][i], c_mask, G, R, FR) for i in range(len(results))]
+            infos=p.map(get_fluo_info, list_V ) #get fluo info ouput mean/max/std for all 3 channels 
+            infos_table=np.array([[j for i in infos[k] for j in i] for k in range(len(infos))])
+            #update the table 
+            infos_table_cell = pd.DataFrame(data=infos_table, columns=['cell','G_mean','G_max','G_std','R_mean', 'R_max','R_std', 'Fr_mean', 'Fr_max', 'Fr_std'])
+            #do the same thing with the nuclear masks 
+            list_V=[(results['cell'][i], n_mask, G, R, FR) for i in range(len(results))]
+            infos=p.map(get_fluo_info, list_V )
+            infos_table=np.array([[j for i in infos[k] for j in i] for k in range(len(infos))])
+            infos_table_nucleus = pd.DataFrame(data=infos_table, columns=['cell','N_G_mean','N_G_max','N_G_std','N_R_mean', 'N_R_max','N_R_std', 'N_Fr_mean', 'N_Fr_max', 'N_Fr_std'])
+
+            #merge all the tables 
+            results = pd.merge(results, infos_table_cell, on="cell", how="left")
+            results = pd.merge(results, infos_table_nucleus, on="cell", how="left")
+
+    else: 
+        results=pd.read_csv(ref_table)
     return results
 
 
 if __name__ == '__main__':
 
     cell_masks=snakemake.input[0]
+   
     nucleus_masks=snakemake.input[1]
     g=snakemake.input[2]
     r=snakemake.input[3]
     fr=snakemake.input[4]
+ 
+    ref_table=snakemake.input[5]
 
     thread_count=snakemake.threads
 
     t=int(g.split("ch2t")[1].split('.tiff')[0])
-    exp=cell_masks.split('/')[0]
-    pos=cell_masks.split('/')[1].split('_mask')[0]
 
-    results= describe(cell_masks, nucleus_masks, t, thread_count, g,r,fr)
+    file_name=cell_masks.split('/results/')[1]
+
+    file_name=file_name.split('/')[0]
+
+    exp=file_name.split('_')[0]
+    pos=file_name.split('_')[1]
+    results= describe(cell_masks, nucleus_masks, t, thread_count, g, r , fr , ref_table)
     col_names=['exp_ID', 'position']+results.columns.to_list()
     results['exp_ID']=[exp for i in range(len(results))]
     results['position']=[pos for i in range(len(results))]
