d2962d7bba4294a784cfbd16b81cb40fa9282329
IrisBP <irisbapi@pm.me>
2026-08-28 10:10:10 +0200

    mods to variables

--- pipeline_local.sh
+++ pipeline_local.sh
@@ -18,13 +18,12 @@ POS='p2rep3_r05c06f02'
 
 #============= Localization Variables =============
 # path to the various directory required for the pipeline 
-WORKDIR="/cluster/scratch/ibarbier/$EXP_DAY/"
-RESULTSDIR="/cluster/scratch/ibarbier/$EXP_DAY/results"
-IMGDIR="/cluster/scratch/ibarbier/$EXP_DAY/Images"
-SOURCEDIR="/nfs/nas22/fs2202/biol_bc_barral_2/ibarbier/2026_GFP_screen/$EXP_DAY/$EXP_DAY/Images/"
-CODEDIR="/cluster/home/ibarbier/Yeast_screening_pipeline/"
+WORKDIR="./$EXP_DAY"
+RESULTSDIR="$WORKDIR/results/"
+IMGDIR="/Volumes/biol_bc_barral_2/ibarbier/2026_GFP_screen/2$EXP_DAY/$EXP_DAY/Images/"
+CODEDIR="./Yeast_screening_pipeline"
 
-ARRAY_PY=/cluster/home/ibarbier/Yeast_screening_pipeline/make_array_file.py
+ARRAY_PY="$CODEDIR/make_array_file.py
 SNAKEFILE=/cluster/home/ibarbier/Yeast_screening_pipeline/pipeline.smk
 #================ Script =======================
 
@@ -51,14 +50,10 @@ if [ ! -d "$RESULTSDIR" ]; then
 fi
 
 # create the array ID table 
-DPC="$IMGDIR/$POS" + "ch1t1.tiff"
-G="$IMGDIR/$POS" + "ch2t1.tiff"
-R="$IMGDIR/$POS" + "ch3t1.tiff"
-FR="$IMGDIR/$POS" + "ch4t1.tiff"
+python $ARRAY_PY $WORKDIR $EXP_ID
 
-C_OUT="$RESULTSDIR/$POS" + "_mask.tiff" 
-N_OUT="$RESULTSDIR/$POS" + "_Nmask.tiff"
-echo $DPC 
-echo $C_OUT
+# submit snakemake 
+# --use-conda --conda-frontend conda
 
-python "/cluster/home/ibarbier/Yeast_screening_pipeline/python_scripts/P1_segmentation.py" $DPC $G $R $FR $C_OUT $N_OUT
+snakemake --cores 35 --use-conda --conda-frontend conda --scheduler greedy --snakefile $SNAKEFILE --config position=$POS codedir=$CODEDIR workdir=$WORKDIR
+
